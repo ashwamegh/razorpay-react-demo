94d6eead6040d20960aac643d8d0942c2f1799f1
Shashank Shekhar <shashank7200@gmail.com>
2017-12-07 13:49:36 +0530

    modified and refractored configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: path.join(__dirname, "src", "index"),
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules'),
-        path.resolve(__dirname, 'bower_components')
-      ],
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015']
-      }
-    }]
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader"
+      },
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ }
+    ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ],
   resolve: {
-    extensions: ['.json', '.js', '.jsx', '.css']
+    extensions: [".json", ".js", ".jsx", ".css"]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
-    publicPath: path.join('/dist/')
+    publicPath: "/"
   }
 };
