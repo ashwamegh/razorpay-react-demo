36171df66de81ba3817415caa6f6c1042e22487d
Shashank Shekhar <shashank7200@gmail.com>
2017-12-08 17:20:31 +0530

    minor output dir rename

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.join(__dirname, "src", "index"),
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "docs")
   },
   module: {
     rules: [
