0973338e6af446be898ad457af74e0b14744f132
shashank7200 <shashank7200@gmail.com>
2022-11-07 23:09:14 +0530

    updated server entry file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('dotenv').load();
+// require('dotenv').load();
 const express = require('express');
 const app = express();
 
@@ -12,43 +12,43 @@ const instance = new Razorpay({
 });
 
 app.all('/*', (req, res, next) => {
-  
-      const allowedReferers = ['http://localhost:8080'];
-  
-      if(allowedReferers.indexOf(req.headers.origin) !== -1){
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-      }else {
-        res.header('Access-Control-Allow-Origin', null);
-      }  
-      res.header('Access-Control-Allow-Credentials', true);
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type,token, Access-Control-Request-Method, Access-Control-Request-Headers');
-      if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-      } else {
-        next();
-      }
+
+  const allowedReferers = ['http://localhost:8080'];
+
+  if (allowedReferers.indexOf(req.headers.origin) !== -1) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  } else {
+    res.header('Access-Control-Allow-Origin', null);
+  }
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type,token, Access-Control-Request-Method, Access-Control-Request-Headers');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 
 
-  app.get('/api/v1/rzp_capture/:payment_id/:amount', (req, res) => {
-    const {payment_id } = req.params;
-    const amount = Number(req.params.amount*100);
-    instance.payments.capture(payment_id, amount).then((data) => {
-      res.json(data);
-    }).catch((error) => {
-      res.json(error);
-    });
+app.get('/api/v1/rzp_capture/:payment_id/:amount', (req, res) => {
+  const { payment_id } = req.params;
+  const amount = Number(req.params.amount * 100);
+  instance.payments.capture(payment_id, amount).then((data) => {
+    res.json(data);
+  }).catch((error) => {
+    res.json(error);
   });
+});
 
-  app.get('/api/v1/rzp_refunds/:payment_id', (req, res) => {
-    const {payment_id} = req.params;
-    instance.payments.refund(payment_id).then((data) => {
-      res.json(data);
-    }).catch((error) => {
-      res.json(error);
-    });
+app.get('/api/v1/rzp_refunds/:payment_id', (req, res) => {
+  const { payment_id } = req.params;
+  instance.payments.refund(payment_id).then((data) => {
+    res.json(data);
+  }).catch((error) => {
+    res.json(error);
   });
+});
 
 app.listen('3000', () => console.log("Listening on port 3000"))
