c74ab6f50b7b205f11b0cc5047322f8ee3601bb0
ashwamegh <shashank7200@gmail.com>
2023-04-12 22:27:31 +0530

    minor changes for vercel deployment

--- server/app.js
+++ server/app.js
@@ -9,17 +9,43 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.use('/', indexRouter);
+const Razorpay = require('razorpay');
+
+const instance = new Razorpay({
+	key_id: process.env.RZP_KEY_ID,
+	key_secret: process.env.RZP_KEY_SECRET
+});
+
+app.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+app.get('/api/v1/rzp_capture/:payment_id/:amount', (req, res) => {
+	const { payment_id } = req.params;
+	const amount = Number(req.params.amount * 100);
+	instance.payments.capture(payment_id, amount).then((data) => {
+		res.json(data);
+	}).catch((error) => {
+		res.json(error);
+	});
+});
+
+app.get('/api/v1/rzp_refunds/:payment_id', (req, res) => {
+	const { payment_id } = req.params;
+	instance.payments.refund(payment_id).then((data) => {
+		res.json(data);
+	}).catch((error) => {
+		res.json(error);
+	});
+});
 
 
 
